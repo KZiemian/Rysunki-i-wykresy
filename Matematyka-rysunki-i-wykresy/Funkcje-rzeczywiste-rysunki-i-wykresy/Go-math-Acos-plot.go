b771aa107787acee77a890b771eb364dbad2197e
Kamil Ziemian <kziemianfvt@gmail.com>
2023-08-25 12:48:31 +0200

    Go graphics 02

--- Matematyka-rysunki-i-wykresy/Funkcje-rzeczywiste-rysunki-i-wykresy/Go-math-Acos-plot.go
+++ Matematyka-rysunki-i-wykresy/Funkcje-rzeczywiste-rysunki-i-wykresy/Go-math-Acos-plot.go
@@ -9,71 +9,24 @@ import (
 )
 
 func main() {
-	mathAcosFunPlotData := make(plotter.XYs, 21)
+	mathAcosFunPlotData := make(plotter.XYs, 601)
 
-	mathAcosFunPlotData[0].X = -1.0
-	mathAcosFunPlotData[0].Y = math.Acos(-1.0)
+	x := -1.0
+	deltaX := 1.0/300.0
 
-	mathAcosFunPlotData[1].X = -0.9
-	mathAcosFunPlotData[1].Y = math.Acos(-0.9)
+	for i := range mathAcosFunPlotData {
+		mathAcosFunPlotData[i].X = x
+		mathAcosFunPlotData[i].Y = math.Acos(x)
 
-	mathAcosFunPlotData[2].X = -0.8
-	mathAcosFunPlotData[2].Y = math.Acos(-0.8)
+		x += deltaX
 
-	mathAcosFunPlotData[3].X = -0.7
-	mathAcosFunPlotData[3].Y = math.Acos(-0.7)
+		if x >= 1.0 {
+			break
+		}
+	}
 
-	mathAcosFunPlotData[4].X = -0.6
-	mathAcosFunPlotData[4].Y = math.Acos(-0.6)
-
-	mathAcosFunPlotData[5].X = -0.5
-	mathAcosFunPlotData[5].Y = math.Acos(-0.5)
-
-	mathAcosFunPlotData[6].X = -0.4
-	mathAcosFunPlotData[6].Y = math.Acos(-0.4)
-
-	mathAcosFunPlotData[7].X = -0.3
-	mathAcosFunPlotData[7].Y = math.Acos(-0.3)
-
-	mathAcosFunPlotData[8].X = -0.2
-	mathAcosFunPlotData[8].Y = math.Acos(-0.2)
-
-	mathAcosFunPlotData[9].X = -0.1
-	mathAcosFunPlotData[9].Y = math.Acos(-0.1)
-
-	mathAcosFunPlotData[10].X = 0.0
-	mathAcosFunPlotData[10].Y = math.Acos(0.0)
-
-	mathAcosFunPlotData[11].X = 0.1
-	mathAcosFunPlotData[11].Y = math.Acos(0.1)
-
-	mathAcosFunPlotData[12].X = 0.2
-	mathAcosFunPlotData[12].Y = math.Acos(0.2)
-
-	mathAcosFunPlotData[13].X = 0.3
-	mathAcosFunPlotData[13].Y = math.Acos(0.3)
-
-	mathAcosFunPlotData[14].X = 0.4
-	mathAcosFunPlotData[14].Y = math.Acos(0.4)
-
-	mathAcosFunPlotData[15].X = 0.5
-	mathAcosFunPlotData[15].Y = math.Acos(0.5)
-
-	mathAcosFunPlotData[16].X = 0.6
-	mathAcosFunPlotData[16].Y = math.Acos(0.6)
-
-	mathAcosFunPlotData[17].X = 0.7
-	mathAcosFunPlotData[17].Y = math.Acos(0.7)
-
-	mathAcosFunPlotData[18].X = 0.8
-	mathAcosFunPlotData[18].Y = math.Acos(0.8)
-
-	mathAcosFunPlotData[19].X = 0.9
-	mathAcosFunPlotData[19].Y = math.Acos(0.9)
-
-	mathAcosFunPlotData[20].X = 1.0
-	mathAcosFunPlotData[20].Y = math.Acos(1.0)
-
+	mathAcosFunPlotData[600].X = 1.0
+	mathAcosFunPlotData[600].Y = math.Acos(1.0)
 
 
 	plotFun := plot.New()
---
+++ Matematyka-rysunki-i-wykresy/Funkcje-rzeczywiste-rysunki-i-wykresy/Go-math-Acosh-plot.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"math"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
+)
+
+func main() {
+	mathAcoshFunPlotData := make(plotter.XYs, 20)
+
+
+	mathAcoshFunPlotData[0].X = 1.0
+	mathAcoshFunPlotData[0].Y = math.Acosh(1.0)
+
+	mathAcoshFunPlotData[1].X = 1.1
+	mathAcoshFunPlotData[1].Y = math.Acosh(1.1)
+
+	mathAcoshFunPlotData[2].X = 1.2
+	mathAcoshFunPlotData[2].Y = math.Acosh(1.2)
+
+	mathAcoshFunPlotData[3].X = 1.3
+	mathAcoshFunPlotData[3].Y = math.Acosh(1.3)
+
+	mathAcoshFunPlotData[4].X = 1.4
+	mathAcoshFunPlotData[4].Y = math.Acosh(1.4)
+
+	mathAcoshFunPlotData[5].X = 1.5
+	mathAcoshFunPlotData[5].Y = math.Acosh(1.5)
+
+	mathAcoshFunPlotData[6].X = 1.6
+	mathAcoshFunPlotData[6].Y = math.Acosh(1.6)
+
+	mathAcoshFunPlotData[7].X = 1.7
+	mathAcoshFunPlotData[7].Y = math.Acosh(1.7)
+
+	mathAcoshFunPlotData[8].X = 1.8
+	mathAcoshFunPlotData[8].Y = math.Acosh(1.8)
+
+
+
+
+
+
+	plotFun := plot.New()
+
+	plotFun.Title.Text = "Wykres funkcji math.Acosh(x)"
+
+	plotFun.X.Label.Text = "x"
+	plotFun.Y.Label.Text = "y"
+
+	err := plotutil.AddLinePoints(plotFun,
+		"math.Acosh", mathAcoshFunPlotData)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := plotFun.Save(10*vg.Inch, 10*vg.Inch,
+		"Go_math_Acosh_plot.png"); err != nil {
+		panic(err)
+	}
+}
